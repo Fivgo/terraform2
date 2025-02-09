ecbbee2371ec6ac0240acb8471d2de1388e07251
Fivgo <thefiveegos@gmail.com>
2025-02-09 09:26:33 -0800

    File Upload Stable

--- hello-world/main.tf
+++ hello-world/main.tf
@@ -8,6 +8,28 @@ terraform {
   }
 }
 
+variable "gcp_bucket" {
+  type = string
+  default = "e-bucket-terraform-built"
+  description = "The name of the GCP bucket to create"
+}
+
+
+# variable "gcp_credentials" {
+#   type = string
+#   sensitive = true
+#   description = "Google Cloud service account credentials"
+# }
+
+
+provider "google" {
+  #credentials = "${file("account.json")}"
+  credentials = file("./application_default_credentials.json")
+  project     = "vital-reef-450000-f4"
+  region      = "us-central1"
+  zone        = "us-central1-c"
+}
+
 resource "null_resource" "default" {
   provisioner "local-exec" {
     command = "echo 'Hello World'"
@@ -18,7 +40,7 @@ resource "null_resource" "default" {
 # location with Standard Storage
 
 resource "google_storage_bucket" "static" {
- name          = "e-bucket-terraform-built"
+ name          = var.gcp_bucket
  location      = "US"
  storage_class = "STANDARD"
 
@@ -30,7 +52,7 @@ resource "google_storage_bucket" "static" {
 
 resource "google_storage_bucket_object" "default" {
  name         = "sample_file.txt"
- source       = "~/terraform/hello-world/sample_file.txt"
+ source       = "./sample_file.txt"
  content_type = "text/plain"
  bucket       = google_storage_bucket.static.id
 }
